946a144008c2e449a5d862ec02ef5d5080db8e68
lgrignon <louis.grignon@protonmail.com>
2023-03-17 15:18:14 +0100

    remove pollution

--- movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/resources/CatalogResource.java
+++ movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/resources/CatalogResource.java
@@ -1,9 +1,8 @@
 package io.javabrains.moviecatalogservice.resources;
 
-import io.javabrains.moviecatalogservice.models.CatalogItem;
-import io.javabrains.moviecatalogservice.models.Movie;
-import io.javabrains.moviecatalogservice.models.Rating;
-import io.javabrains.moviecatalogservice.models.UserRating;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,10 +10,9 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.stream.Collectors;
+import io.javabrains.moviecatalogservice.models.CatalogItem;
+import io.javabrains.moviecatalogservice.models.Movie;
+import io.javabrains.moviecatalogservice.models.UserRating;
 
 @RestController
 @RequestMapping("/catalog")
